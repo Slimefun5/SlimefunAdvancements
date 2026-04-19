580c6a9fe7b270dac43ac2d765c9dde400c21fdf
Intisy <finn@birich.de>
2026-04-19 10:49:11 +0200

    Migrate project build from Maven to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "9.3.2"
+    id("jacoco")
+}
+
+group = "me.char321"
+version = "MODIFIED"
+description = "Slimefun Advancements is a Slimefun addon adding an advancement system."
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+    compileOnly(fileTree("${rootDir}/../Slimefun5/build/libs") { include("*.jar") })
+
+    // Shaded libraries
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+    implementation("com.github.qwertyuioplkjhgfd:AdvancementAPI:f243bdaf75") {
+        isTransitive = false
+    }
+    implementation("com.github.baked-libs.dough:dough-api:1108163a49") {
+        isTransitive = false
+    }
+
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-core:5.15.2")
+    testImplementation("org.slf4j:slf4j-simple:2.0.16")
+    testImplementation("org.mockbukkit.mockbukkit:mockbukkit-v1.21:4.107.0") {
+        exclude(group = "org.jetbrains", module = "annotations")
+    }
+}
+
+configurations.testImplementation {
+    extendsFrom(configurations.compileOnly.get())
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+    jar {
+        enabled = false
+    }
+    shadowJar {
+        archiveFileName.set("sfadvancements-${project.version}.jar")
+        relocate("org.bstats", "me.char321.sfadvancements.libs.bstats")
+        relocate("net.roxeez.advancement", "me.char321.sfadvancements.libs.advancementapi")
+        relocate("io.github.bakedlibs.dough", "me.char321.sfadvancements.libs.dough")
+        exclude("META-INF/**")
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+    test {
+        useJUnitPlatform()
+        finalizedBy(jacocoTestReport)
+    }
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.required.set(true)
+        }
+    }
+}
