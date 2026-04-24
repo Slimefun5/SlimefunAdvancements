0d7c627b358dffc7702c77a9bb7b4f533192acc5
Intisy <finn@birich.de>
2026-04-24 21:39:07 +0200

    Add github-gradle plugin for dependency management and release publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
 plugins {
     java
     id("com.gradleup.shadow") version "9.3.2"
+    id("io.github.intisy.github-gradle") version "1.3.8"
     id("jacoco")
 }
 
 group = "me.char321"
 version = "MODIFIED"
 description = "Slimefun Advancements is a Slimefun addon adding an advancement system."
+
+github {
+    accessToken = System.getenv("GITHUB_TOKEN") ?: ""
+}
+
+publishGithub {
+    tag = System.getenv("GITHUB_REF_NAME")
+}
 
 java {
     toolchain {
@@ -25,7 +34,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
-    compileOnly(fileTree("${rootDir}/../Slimefun5/build/libs") { include("*.jar") })
+    githubCompileOnly("Slimefun5:Slimefun5:v5.0.3")
 
     // Shaded libraries
     implementation("org.bstats:bstats-bukkit:3.0.2")
