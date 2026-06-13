562851d46671c7901c776c606280684d132b808b
intisy (Finn Birich) <finn@birich.de>
2026-06-14 01:04:23 +0200

    build(ci): consume core via github-gradle (Slimefun5:Slimefun5:v5.2.1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("com.github.Slimefun5:SlimefunMetrics:master-SNAPSHOT")
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
-    compileOnly(files("../../core/Slimefun5/core/build/libs/Slimefun v5.0.0-UNOFFICIAL-MC26.1.2.jar"))
+    githubCompileOnly("Slimefun5:Slimefun5:v5.2.1")
 
         implementation("com.github.qwertyuioplkjhgfd:AdvancementAPI:f243bdaf75") {
         isTransitive = false
@@ -67,6 +67,7 @@ tasks {
         enabled = false
     }
     shadowJar {
+        relocate("org.bstats", "slimefunadvancements.libs.bstats")
         archiveFileName.set("SlimefunAdvancements v${project.version}-MC26.1.2.jar")
                 relocate("net.roxeez.advancement", "me.char321.sfadvancements.libs.advancementapi")
         exclude("META-INF/**")
