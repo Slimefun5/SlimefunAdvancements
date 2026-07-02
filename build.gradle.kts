e74e102a801d53329c57c7c34b5f98a076035302
intisy (Finn Birich) <finn@birich.de>
2026-07-02 11:58:41 +0200

    fix(build): consume SlimefunMetrics via github-gradle (v1.0.0), drop jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.Slimefun5:SlimefunMetrics:master-SNAPSHOT")
+    githubImplementation("Slimefun5:SlimefunMetrics:v1.0.0")
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     githubCompileOnly("Slimefun5:Slimefun5:gh-v5.2.3.2")
